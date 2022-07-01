e2b24f47a0c0b347d2781df43744c2736f8a1d49
ntim <timotic.nikola.27@gmail.com>
2022-07-01 20:01:43 +0200

    add array_methods

---
+++ array_methods.js
@@ -0,0 +1,60 @@
+//POP
+// let arr = [1, 2, 3]
+
+// let poslednji = arr.pop()
+// console.log(arr.pop())
+
+//PUSH
+
+// console.log(arr.push(4), arr)
+
+//SHIFT
+
+// console.log(arr.shift(), arr)
+
+//UNSHIFT
+
+// console.log(arr.unshift(1,2,3), arr);
+
+// CONCAT
+// arr.concat()
+// SPREAD OPERATOR
+// let newArray = [...arr, ...newArr]
+
+// for(let i = 0; i < arr.length; i++) {
+//     newArray.push(arr[i])
+// }
+
+// JOIN
+
+let arr = ['a' , 'b', 'c']
+
+// console.log(arr.join())
+
+// let str = "STRING JE NAJLAKSI TIP PODATKA AKO JE U FORMATU JSON-a"
+
+// console.log(str.split(" "))
+// str.join
+
+// SLICE
+
+// console.log(arr.slice(-1), arr);
+
+// SPLICE
+
+// let objekat = {
+//     ime,
+//     prezime,
+//     godine
+// }
+
+// let deleted = arr.splice(1, 1, objekat)
+
+// console.log(arr, deleted);
+
+//INDEXOF
+
+// arr.indexOf('b')
+
+// if(arr.indexOf('b') == -1) return
+
