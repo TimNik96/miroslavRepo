22bc098d03a25e2953c4a8a2f53ec3bc95bb9783
ntim <timotic.nikola.27@gmail.com>
2022-08-18 00:30:37 +0200

    delete excess code from spreadOperator.js

--- js/spreadOperator.js
+++ js/spreadOperator.js
@@ -27,27 +27,3 @@
 // } catch (err){
 //     if(err instanceof AxiosError)
 // }
-
-const inputNumOfDivs = document.createElement('input')
-inputNumOfDivs.type = 'number'
-const pExplanation = document.createElement('p')
-pExplanation.textContent = 'Uneti zeljeni broj paragrafa:'
-
-const body = document.body
-
-body.append(pExplanation, inputNumOfDivs)
-
-parseInt(inputNumOfDivs.value)
-
-for(let i = 0; i < +inputNumOfDivs.value; i++) {
-
-}
-
-
-// let str = ' 20'
-
-// if(typeof +str !== number)
-//     return
-
-// console.log(typeof str);
-// console.log(parseInt(str.trim()));
