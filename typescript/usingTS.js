b13100835fa7916533e8e159e853a52a31b640c0
ntim <timotic.nikola.27@gmail.com>
2022-12-20 22:08:21 +0100

    finish core types

---
+++ typescript/usingTS.js
@@ -0,0 +1,20 @@
+// JS
+//number
+//boolean
+//string
+//null
+//undefined
+//symbol
+var obj = {
+    name: 'Marko',
+    age: 31,
+    outputType: "as-text",
+    hobbies: ['Kosarka', 'Fudbal'],
+    pozicija: ['play', 1]
+};
+// for(const hobby of obj.hobbies) {
+//     console.log(hobby);
+// }
+// let niz: string[]
+obj.pozicija.push('item');
+console.log(obj.pozicija);
--- typescript/usingTS.ts
+++ typescript/usingTS.ts
@@ -1,3 +1,4 @@
+// JS
 //number
 //boolean
 //string
@@ -9,18 +10,79 @@
     //array
     //function
 
-const inputNum1 = document.querySelector('#num1')! as HTMLInputElement
-const inputNum2 = document.querySelector('#num2')! as HTMLInputElement
-const button = document.querySelector('button')
+//TS
+//number
+//string
+//boolean
+//object
+//array
+//tuple
+//enum
+//any
+//unknown
+//never
+
+//custom type
+//union type
+//literal type
+
+// const inputNum1 = document.querySelector('#num1')! as HTMLInputElement
+// const inputNum2 = document.querySelector('#num2')! as HTMLInputElement
+// const button = document.querySelector('button')
 
 // const num1: number = +inputNum1.value
 // const nmb = 5
 
 // console.log(inputNum1.value)
 
-const saberi = (number_1: number, number_2: number) => {
-    return number_1 + number_2
+// const saberi = (number_1: number, number_2: number) => {
+//     return number_1 + number_2
+// }
+
+// console.log(saberi(10, 20))
+
+// const promenljiva: unknown = 5
+// // promenljiva = 5
+// promenljiva = 'nmsakldmasdk'
+
+// let num: unknown;
+// num = 5
+// let str = 
+
+class P {
+    name: string;
+    age: string;
+    
+}
+type Combined = string | null
+
+type MyObj = {
+    name: string;
+    age: number;
+    // mode: [string, number];
+    outputType: 'as-number' | 'as-text';
+    hobbies?: Combined[];
+    pozicija?: [string, number];
 }
 
-console.log(saberi(10, 20))
+const obj: MyObj = {
+    name: 'Marko',
+    age: 31,
+    outputType: "as-text",
+    // hobbies: ['Kosarka', 'Fudbal'],
+    pozicija: ['play', 1]
+}
+
+// for(const hobby of obj.hobbies) {
+//     console.log(hobby);
+// }
+
+// let niz: string[]
+
+
+// obj.pozicija.push('item')
+console.log(obj.pozicija)
+
+
+
 
